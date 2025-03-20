90a50d96d25214852ca3e4033584f557ce768041
PavanKalyan96Dev <mallempavankalyan96@gmail.com>
2025-03-20 09:49:02 +0530

    Add files via upload

---
+++ Day 13/Docker/springboot-docker-demo/springboot-docker-demo/src/main/java/com/wipro/SpringbootDockerDemoApplication.java
@@ -0,0 +1,11 @@
+package com.wipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerDemoApplication.class, args);
+    }
+} 
